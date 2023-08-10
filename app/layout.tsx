785fe4f8f3ea29632978448e772a3aae2f53d901
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-08-10 15:04:26 +0900

    chore: `@tanstack/react-query` 설치 및 provider 컴포넌트 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import StyledComponentsRegistry from './_lib/registry';
 import fonts from './_styles/fonts';
 import './_styles/global.css';
+import ReactQueryProvider from './_components/providers/ReactQueryProvider';
 
 export const metadata: Metadata = {
   title: 'Lottofolio',
@@ -10,13 +11,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html className={fonts.className}>
+    <html lang="ko" className={fonts.className}>
       <body>
-        <StyledComponentsRegistry>
-          <p>header layout</p>
-          {children}
-          <p>footer layout</p>
-        </StyledComponentsRegistry>
+        <ReactQueryProvider>
+          <StyledComponentsRegistry>
+            <p>header layout</p>
+            {children}
+            <p>footer layout</p>
+          </StyledComponentsRegistry>
+        </ReactQueryProvider>
       </body>
     </html>
   );
