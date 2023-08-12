723fc148a2dd25a4078a2450a9b807c15e33c202
MINJEONG <96714229+lxxmnmn@users.noreply.github.com>
2023-08-12 11:36:29 +0900

    chore: 폴더 구조 변경 및 svg 컴포넌트 설정
    * chore: types, assets 폴더 구조 변경
    * chore: svg 컴포넌트 임시 생성

---
+++ app/_constants/index.ts
@@ -0,0 +1,10 @@
+export const ERROR = {
+  NETWORK: {
+    title: '네트워크 상태를 확인해주세요',
+    description: '서버와의 통신이 원활하지 않아 데이터를 불러올 수 없습니다.',
+  },
+  COMMON: {
+    title: '일시적인 오류입니다',
+    description: '서비스 이용에 불편을 드려서 죄송합니다. 잠시 후 재시도해주세요.',
+  },
+};
--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
