92467c183fc9d5ab0784f98e7792d23b77d441dc
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-08-16 18:22:25 +0900

    fix(IconKakao): 속성명 변경

--- app/_components/icons/IconKakao.tsx
+++ app/_components/icons/IconKakao.tsx
@@ -2,11 +2,11 @@ const IconKakao = () => {
   return (
     <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M12.7692 4C8.13231 4 4 7.2082 4 11.1656C4 13.6262 5.59846 15.7964 8.03231 17.0867L7.00821 20.8456C6.91744 21.1785 7.2959 21.4431 7.58615 21.2503L12.0754 18.2733C12.4538 18.3097 12.839 18.3313 12.7692 18.3313C18.3287 18.3313 22 15.1226 22 11.1656C22 7.2082 18.3287 4 12.7692 4Z"
         fill="black"
-        fill-opacity="0.902"
+        fillOpacity="0.902"
       />
     </svg>
   );
