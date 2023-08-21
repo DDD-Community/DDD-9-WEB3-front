31e2d977a3b3415199fc217cf21480f0361c5a48
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-08-22 00:36:14 +0900

    feat: 공통 버튼 컴포넌트 구현

--- app/_components/common/Button.tsx
+++ app/_components/common/Button.tsx
@@ -1,16 +1,48 @@
-import React from 'react';
+import palette from '@styles/palette';
+import type { CSSProperties, PropsWithChildren, ComponentPropsWithoutRef } from 'react';
 import styled from 'styled-components';
 
-type ButtonProps = {};
+const SIZE_STYLE = {
+  medium: '9.6rem',
+  full: '100%',
+};
 
-const Button: React.FC<ButtonProps> = () => {
+type Size = keyof typeof SIZE_STYLE;
+
+interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
+  size?: Size;
+  color?: CSSProperties['color'];
+  $backgroundColor?: CSSProperties['backgroundColor'];
+}
+
+const Button = ({
+  children,
+  size = 'full',
+  color = palette.white,
+  $backgroundColor = palette.orange_30,
+  ...props
+}: PropsWithChildren<ButtonProps>) => {
   return (
-    <ButtonBlock>
-      <p>button</p>
-    </ButtonBlock>
+    <Wrapper size={SIZE_STYLE[size]} color={color} $backgroundColor={$backgroundColor} {...props}>
+      {children}
+    </Wrapper>
   );
 };
 
-const ButtonBlock = styled.div``;
-
 export default Button;
+
+const Wrapper = styled.button<Omit<ButtonProps, 'size'> & { size: string }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: ${({ size }) => size};
+  padding: 0.62rem 0;
+  background: ${({ $backgroundColor }) => $backgroundColor};
+  color: ${({ color }) => color};
+  border-radius: 0.5rem;
+  font-size: 1rem;
+  font-weight: 700;
+  line-height: 150%;
+  letter-spacing: -0.008rem;
+  cursor: pointer;
+`;
