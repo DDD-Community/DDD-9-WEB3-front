1e4282351d8abf68cb3c4d8706d2e20e646ff988
heeyeon <junghy14971@gmail.com>
2023-08-22 00:49:34 +0900

    feat: 막대차트 제작 및 시작일,종료일 필터 ui 제작

--- app/_components/analysis/chart/BarChartWrapper.tsx
+++ app/_components/analysis/chart/BarChartWrapper.tsx
@@ -1,51 +1,107 @@
 import React from 'react';
-import { Bar } from 'react-chartjs-2';
 import styled from 'styled-components';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import ArrowIcon from '@assets/svg/arrow.svg';
 import palette from '@/_styles/palette';
-
-const BarChartWrapperBlock = styled.div``;
 
 type BarChartWrapperProps = {};
 
 const BarChartWrapper: React.FC<BarChartWrapperProps> = () => {
-  ChartJS.register(ArcElement, Tooltip);
-
-  const data = {
-    scales: {
-      x: {
-        beginAtZero: true,
-        grid: {
-          display: false, // x축의 그리드 라인 제거
-        },
-      },
-      y: {
-        beginAtZero: true,
-      },
-    },
-    indexAxis: 'y',
-  };
+  const numbers = Array.from({ length: 45 }, (_, index) => index + 1);
 
   return (
     <BarChartWrapperBlock>
-      {/* <Bar
-        data={data}
-        options={{
-          scales: {
-            x: {
-              display: false, // x축의 그리드 제거
-              beginAtZero: true,
-            },
-            y: {
-              // display: false, // x축의 그리드 제거
-              beginAtZero: true,
-            },
-          },
-          indexAxis: 'y', // y축을 기준으로 차트를 그림
-        }}
-      /> */}
+      <NumberBar>
+        <PrevButton>
+          <ArrowIcon />
+        </PrevButton>
+        <Text>1 ~ 10</Text>
+        <NextButton>
+          <ArrowIcon />
+        </NextButton>
+      </NumberBar>
+
+      <BarList>
+        {numbers.map(num => (
+          <BarItem key={num}>
+            <Num>{num}</Num>
+            <StatisticalBar test={50} />
+          </BarItem>
+        ))}
+      </BarList>
     </BarChartWrapperBlock>
   );
 };
+
+const BarChartWrapperBlock = styled.div`
+  margin: 18px 0;
+`;
+
+const NumberBar = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 19px;
+`;
+
+const Text = styled.p`
+  font-weight: bold;
+`;
+
+const NextButton = styled.button`
+  display: flex;
+  align-items: center;
+  border: none;
+  background-color: transparent;
+`;
+
+const PrevButton = styled(NextButton)`
+  transform: rotate(-180deg);
+`;
+
+const BarList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 27px;
+`;
+
+const BarItem = styled.li`
+  display: flex;
+`;
+
+const Num = styled.span`
+  color: #979da6;
+  font-size: 14px;
+  font-weight: 600;
+`;
+
+const StatisticalBar = styled.div<{ test: number }>`
+  @keyframes barAni {
+    from {
+      width: 0;
+    }
+    to {
+      width: ${props => props.test && props.test * 3.38}px;
+    }
+  }
+
+  width: 100%;
+  height: 12px;
+  background-color: #eff3f8;
+  position: relative;
+  border-radius: 6px;
+  overflow: hidden;
+  margin-left: 15px;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    height: 100%;
+    width: 0;
+    border-radius: 6px;
+    background-color: ${palette.blue_15};
+    animation: ${({ test }) => test && `barAni 0.8s forwards`};
+  }
+`;
 
 export default BarChartWrapper;
--- app/_components/analysis/period/pick/PeriodicDatePicker.tsx
+++ app/_components/analysis/period/pick/PeriodicDatePicker.tsx
@@ -1,18 +1,75 @@
 'use client';
 
+import palette from '@/_styles/palette';
 import React from 'react';
 import styled from 'styled-components';
+import CalendarFilterIcon from '@assets/svg/calendarFilter.svg';
+import DashIcon from '@assets/svg/dash.svg';
 
 type PeriodicDatePickerProps = {};
 
 const PeriodicDatePicker: React.FC<PeriodicDatePickerProps> = () => {
   return (
     <PeriodicDatePickerBlock>
-      <p>날짜 선택</p>
+      <div>
+        <LabelText>시작일</LabelText>
+        <DateBox>
+          <PickedDate>2022.05</PickedDate>
+          <button>
+            <CalendarFilterIcon />
+          </button>
+        </DateBox>
+      </div>
+
+      <DashIconSVG />
+
+      <div>
+        <LabelText>종료일</LabelText>
+        <DateBox>
+          <PickedDate>2022.05</PickedDate>
+          <button>
+            <CalendarFilterIcon />
+          </button>
+        </DateBox>
+      </div>
     </PeriodicDatePickerBlock>
   );
 };
 
-const PeriodicDatePickerBlock = styled.div``;
+const PeriodicDatePickerBlock = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background-color: ${palette.blue_ligth};
+  padding: 28px 20px;
+  position: relative;
+`;
+
+const DashIconSVG = styled(DashIcon)`
+  position: absolute;
+  bottom: 30px;
+`;
+
+const LabelText = styled.p`
+  font-size: 12px;
+  margin-bottom: 4px;
+  color: ${palette.grey_30};
+`;
+
+const PickedDate = styled.p`
+  font-weight: 500;
+  font-size: 16px;
+`;
+
+const DateBox = styled.div`
+  border: 1px solid ${palette.grey_70};
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 120px;
+  background-color: ${palette.white};
+  border-radius: 5px;
+`;
 
 export default PeriodicDatePicker;
