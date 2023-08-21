9b02b099cc5cb585037408f62a524227b04e7782
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-08-22 01:55:08 +0900

    refactor: 경로 수정

--- app/(router)/login/page.tsx
+++ app/(router)/login/page.tsx
@@ -2,8 +2,8 @@ import LoginPage from '@features/LoginPage';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
-import { AUTH_TOKEN } from '@/_constants/auth';
-import { ROUTES } from '@/_constants/routes';
+import { AUTH_TOKEN } from '@constants/auth';
+import { ROUTES } from '@constants/routes';
 
 export default function Page() {
   if (cookies().get(AUTH_TOKEN.ACCESS)) {
