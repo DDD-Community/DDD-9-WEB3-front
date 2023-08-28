d2a5d73fe78cca1c93f433b0bad61eeeed480dd9
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-08-28 19:28:01 +0900

    feat: 기존 회원인 경우, 회원가입 페이지 접근 막기

--- app/(router)/signup/page.tsx
+++ app/(router)/signup/page.tsx
@@ -1,5 +1,13 @@
+import { ROUTES } from '@constants/routes';
 import SignupPage from '@features/SignupPage';
+import { LOTTOFOLIO_USER } from '@lib/util/storage';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 export default function Page() {
+  if (cookies().get(LOTTOFOLIO_USER)) {
+    redirect(ROUTES.HOME);
+  }
+
   return <SignupPage />;
 }
