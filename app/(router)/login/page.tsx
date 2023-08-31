39407922336b600f74526a614e7723fa7050b39e
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-08-31 19:23:09 +0900

    feat: 로그인 상태일 때 로그인 페이지 이동 막기

--- app/(router)/login/page.tsx
+++ app/(router)/login/page.tsx
@@ -1,11 +1,11 @@
-import { AUTH_TOKEN } from '@constants/auth';
+import { LOTTOFOLIO_USER } from '@constants/auth';
 import { ROUTES } from '@constants/routes';
 import LoginPage from '@features/LoginPage';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 export default function Page() {
-  if (cookies().get(AUTH_TOKEN.ACCESS)) {
+  if (cookies().get(LOTTOFOLIO_USER)) {
     redirect(ROUTES.HOME);
   }
 
