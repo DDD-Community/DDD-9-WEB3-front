55eeae7d3a254fe45c0f634463de8dc1d4a07ae2
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-09-07 02:25:48 +0900

    feat: Bottom Sheet 컴포넌트 사용 예제 코드 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
+'use client';
+
+import { useState } from 'react';
+
+import { Button } from './_components/common';
+import BottomSheet from './_components/common/BottomSheet';
 import Navigation from './_components/common/Navigation';
 import AuthProvider from './_components/providers/AuthProvider';
 
 export default function Home() {
+  const [isOpenScrapBottomSheet, setIsOpenScrapBottomSheet] = useState(false);
+
   return (
-    <AuthProvider>
-      <Navigation />
-    </AuthProvider>
+    <>
+      <AuthProvider>
+        <Navigation />
+      </AuthProvider>
+      <Button onClick={() => setIsOpenScrapBottomSheet(true)}>Bottom Sheet</Button>
+      <BottomSheet
+        isOpen={isOpenScrapBottomSheet}
+        onOpen={() => setIsOpenScrapBottomSheet(true)}
+        onClose={() => setIsOpenScrapBottomSheet(false)}
+      />
+    </>
   );
 }
